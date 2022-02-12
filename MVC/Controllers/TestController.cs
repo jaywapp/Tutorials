714c6ab79cd00f73c501ea38ef37e2abff3f38e3
Jaywapp <jaywapp16@gmail.com>
2022-02-12 11:06:40 +0900

    update MVC Tutorial

---
+++ MVC/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using MVC.Models;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Update()
+        {
+            var person = new Person()
+            {
+                Name = "Jay",
+                Address = "Seoul",
+                Age = 30,
+                Major = "Computer Science",
+            };
+            
+            return View(person);
+        }
+    }
+}
