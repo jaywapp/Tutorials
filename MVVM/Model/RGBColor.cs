1e89c446a93a0984a6fe1148e58950079073b850
jaywapp <jaywapp16@gmail.com>
2022-02-24 10:39:17 +0900

    update mvvm tutorial

---
+++ MVVM/Model/RGBColor.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+namespace mvvm.Model
+{
+    public class RGBColor
+    {
+        #region Properties
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+        #endregion
+    }
+}
