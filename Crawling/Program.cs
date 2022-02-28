9a7ada6e38e12ba623ab25abb467a9ac7e4d3c10
Junyoung Park <jaywapp16@gmail.com>
2022-02-28 17:22:32 +0900

    Add files via upload

---
+++ Crawling/Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrawlingText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var title = GetTitle();
+
+            Console.WriteLine(title);
+            Console.ReadLine();
+        }
+
+        private static string GetTitle()
+        {
+            var driver = new ChromeDriver();
+            driver.Url = "https://sports.news.naver.com/index";
+
+            var element = driver.FindElement(By.ClassName("today_item")).FindElement(By.TagName("a"));
+            var title = element.GetAttribute("title");
+
+            return title;
+        }
+
+    }
+}
