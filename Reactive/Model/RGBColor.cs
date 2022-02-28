b0ee8df39623a44e6d6d2c999068a99cd08bff5e
jaywapp <jaywapp16@gmail.com>
2022-02-28 15:05:22 +0900

    update ReactiveUI tutorial

---
+++ Reactive/Model/RGBColor.cs
@@ -0,0 +1,11 @@
+﻿namespace Reactive.Model
+{
+    public class RGBColor
+    {
+        #region Properties
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+        #endregion
+    }
+}
---
+++ Reactive/View/ColorViewModel.cs
@@ -0,0 +1,58 @@
+﻿using ReactiveUI;
+using System.Reactive.Linq;
+using System.Windows.Media;
+
+namespace Reactive.View
+{
+    public class ColorViewModel : ReactiveObject
+    {
+        #region Internal Field
+        private byte _rValue;
+        private byte _gValue;
+        private byte _bValue;
+
+        private ObservableAsPropertyHelper<Color> _color;
+        private ObservableAsPropertyHelper<string> _colorText;
+        private ObservableAsPropertyHelper<SolidColorBrush> _colorBrush;
+        #endregion
+
+        #region Properties
+        public byte RValue
+        {
+            get => _rValue;
+            set => this.RaiseAndSetIfChanged(ref _rValue, value);
+        }
+        public byte GValue
+        {
+            get => _gValue;
+            set => this.RaiseAndSetIfChanged(ref _gValue, value);
+        }
+        public byte BValue
+        {
+            get => _bValue;
+            set => this.RaiseAndSetIfChanged(ref _bValue, value);
+        }
+
+        public Color Color => _color.Value;
+        public string ColorText => _colorText.Value;
+        public SolidColorBrush ColorBrush => _colorBrush.Value;
+        #endregion
+
+        #region Constructor
+        public ColorViewModel()
+        {
+            this.WhenAnyValue(x => x.RValue, x => x.GValue, x => x.BValue)
+                .Select(p => Color.FromRgb(p.Item1, p.Item2, p.Item3))
+                .ToProperty(this, x => x.Color, out _color);
+
+            this.WhenAnyValue(x => x.Color)
+                .Select(c => c.ToString())
+                .ToProperty(this, x => x.ColorText, out _colorText);
+
+            this.WhenAnyValue(x => x.Color)
+                .Select(c => new SolidColorBrush(c))
+                .ToProperty(this, x => x.ColorBrush, out _colorBrush);
+        }
+        #endregion
+    }
+}
